abbc6eed6384958e9bef99eb137a836b016718c4
fadyMarty <antoshka1234098@gmail.com>
2026-04-14 19:04:39 +0300

    Add edge to edge support for list

--- app/src/main/java/com/fadymarty/jsonmapping/presentation/pokemon_list/PokemonListScreen.kt
+++ app/src/main/java/com/fadymarty/jsonmapping/presentation/pokemon_list/PokemonListScreen.kt
@@ -2,6 +2,8 @@ package com.fadymarty.jsonmapping.presentation.pokemon_list
 
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.calculateEndPadding
+import androidx.compose.foundation.layout.calculateStartPadding
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
@@ -10,6 +12,7 @@ import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalLayoutDirection
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import com.fadymarty.jsonmapping.presentation.pokemon_list.components.CategoryHeader
@@ -29,12 +32,18 @@ fun PokemonListRoot(
 private fun PokemonListScreen(
     state: PokemonListState,
 ) {
+    val layoutDirection = LocalLayoutDirection.current
+
     Scaffold { innerPadding ->
         LazyColumn(
             modifier = Modifier
                 .fillMaxSize()
-                .padding(innerPadding),
-            contentPadding = PaddingValues(bottom = 32.dp),
+                .padding(top = innerPadding.calculateTopPadding()),
+            contentPadding = PaddingValues(
+                start = innerPadding.calculateStartPadding(layoutDirection),
+                end = innerPadding.calculateEndPadding(layoutDirection),
+                bottom = innerPadding.calculateBottomPadding() + 32.dp
+            ),
             verticalArrangement = Arrangement.spacedBy(32.dp)
         ) {
             state.categories.forEach { category ->
