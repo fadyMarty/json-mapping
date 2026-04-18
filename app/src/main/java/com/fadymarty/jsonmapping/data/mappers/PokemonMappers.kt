1fc901c1c0e9dc1fa4a7f71917e86cc2123603f5
fadyMarty <antoshka1234098@gmail.com>
2026-04-18 09:03:19 +0300

    Add toRequestBody mapper for PokemonDto

--- app/src/main/java/com/fadymarty/jsonmapping/data/mappers/PokemonMappers.kt
+++ app/src/main/java/com/fadymarty/jsonmapping/data/mappers/PokemonMappers.kt
@@ -4,13 +4,27 @@ import com.fadymarty.jsonmapping.common.util.Constants
 import com.fadymarty.jsonmapping.data.data_source.remote.dto.PokemonDto
 import com.fadymarty.jsonmapping.domain.model.Pokemon
 import kotlinx.serialization.json.JsonObject
+import kotlinx.serialization.json.buildJsonObject
 import kotlinx.serialization.json.jsonPrimitive
+import kotlinx.serialization.json.put
+import okhttp3.MediaType.Companion.toMediaType
+import okhttp3.RequestBody
+import okhttp3.RequestBody.Companion.toRequestBody
 
 fun JsonObject.toPokemonDto(): PokemonDto {
     return PokemonDto(
         name = this["name"]!!.jsonPrimitive.content,
         url = this["url"]!!.jsonPrimitive.content
     )
+}
+
+fun PokemonDto.toRequestBody(): RequestBody {
+    return buildJsonObject {
+        put("name", name)
+        put("url", url)
+    }
+        .toString()
+        .toRequestBody("application/json".toMediaType())
 }
 
 fun PokemonDto.toPokemon(): Pokemon {
